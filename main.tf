b578118a828a0f69e478efb402161b330e35b645
Akash Biswas <119995028+biswasak32@users.noreply.github.com>
2023-02-02 01:18:57 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -69,3 +69,20 @@ resource "aws_elb" "bar" {
   }
 }
 
+resource "aws_s3_bucket" "b" {
+  bucket = "bucketforterraformcode"
+  acl    = "private"
+
+  versioning {
+    enabled = true
+  }
+}
+
+resource "aws_ebs_volume" "example" {
+  availability_zone = "ap-south-1a"
+  size              = 40
+
+  tags = {
+    Name = "akash-ebs"
+  }
+}
